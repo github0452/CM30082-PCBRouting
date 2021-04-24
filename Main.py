b542e6ca99aea2b3340335fd6c640d5f02c5d780
nlc29 <42607780+github0452@users.noreply.github.com>
2021-04-24 11:48:14 +0100

    Syntax error

--- Main.py
+++ Main.py
@@ -158,6 +158,6 @@ print("Number of epochs: {0}".format(N_EPOCHS))
 for epoch in range(0, N_EPOCHS):
     if purpose == "test":
         agent.test(N_NODES)
-    elif purpose == "train"
+    elif purpose == "train":
         agent.train(N_NODES)#, path=file)
 # agent.save()
